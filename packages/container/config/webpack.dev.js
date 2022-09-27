b8fc37f8b6255c99f45158d66cd8222e70fcf10c
Adolfo Unturbe <apu314@gmail.com>
2022-09-27 13:27:47 +0100

    Webpack production builds for container and marketing apps

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
@@ -7,29 +6,26 @@ const packageJson = require('../package.json')
 packageJson.dependencies
 
 const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: 'index.html'
-    }
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'container', // Name for host is not used
-      remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-      },
-      /* shared: [
+    mode: "development",
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: "index.html",
+        },
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "container", // Name for host is not used
+            remotes: {
+                marketing: "marketing@http://localhost:8081/remoteEntry.js",
+            },
+            /* shared: [
         'react',
         'react-dom'
       ] */
-      shared: packageJson.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
-}
+            shared: packageJson.dependencies,
+        }),
+    ],
+};
 
 module.exports = merge(commonConfig, devConfig)
