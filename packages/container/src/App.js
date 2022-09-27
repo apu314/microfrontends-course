b80108c71f10387be3814606b19cdf40bcdba77a
Adolfo Unturbe <apu314@gmail.com>
2022-09-27 17:03:10 +0100

    [Final] Feature #1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp'
 export default () => {
   return (
     <div>
-      <h1>Hiya!</h1>
+      <h1>Agüita! 🇮🇨</h1>
       <hr />
       <MarketingApp />
     </div>
