5fbe34e0c3b35dbfb70d41a400777681b22ce73f
Adolfo Unturbe <apu314@gmail.com>
2022-09-27 23:34:08 +0100

    Prefix style classes per package

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -6,10 +8,11 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import MaterialLink from '@material-ui/core/Link';
-import { Link } from 'react-router-dom';
+
+import { makeStyles } from '@material-ui/core/styles';
+
 
 function Copyright() {
   return (
@@ -67,7 +70,7 @@ export default function Album() {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
+    <>
       <main>
         {/* Hero unit */}
         <div className={classes.heroContent}>
@@ -161,6 +164,6 @@ export default function Album() {
         <Copyright />
       </footer>
       {/* End footer */}
-    </React.Fragment>
+    </>
   );
 }
