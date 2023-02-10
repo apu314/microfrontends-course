17255f88c5cde73bb3279d2a5ba9037be5eb726c
Adolfo Unturbe <apu314@gmail.com>
2023-02-10 00:05:51 +0000

    add authentication, fix path bugs, lazy loading micro-frontend, add progressbar

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,23 +1,26 @@
-const { merge } = require('webpack-merge')
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
-const commonConfig = require('./webpack.common')
-const packageJson = require('../package.json')
-
-packageJson.dependencies
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
     mode: "development",
+    output: {
+      publicPath: 'http://localhost:8080/'
+    },
     devServer: {
         port: 8080,
-        historyApiFallback: {
-            index: "index.html",
-        },
+        // historyApiFallback: {
+        //     index: "index.html",
+        // },
+        historyApiFallback: true,
     },
     plugins: [
         new ModuleFederationPlugin({
             name: "container", // Name for host is not used
             remotes: {
                 marketing: "marketing@http://localhost:8081/remoteEntry.js",
+                auth: "auth@http://localhost:8082/remoteEntry.js"
             },
             /* shared: [
         'react',
@@ -28,4 +31,4 @@ const devConfig = {
     ],
 };
 
-module.exports = merge(commonConfig, devConfig)
+module.exports = merge(commonConfig, devConfig);
